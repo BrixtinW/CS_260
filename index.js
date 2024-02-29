65a8a5f86fff5b3cf35ebcf5b4428bd4f77aaf1c
BrixtinW <brixtinlwalker@gmail.com>
2024-02-28 18:59:51 -0700

    submit words now works

--- index.js
+++ index.js
@@ -8,5 +8,31 @@
     console.log(users);
 
 
+    let secretWordPairs = [
+        ["secret", "Octopus"],
+        ["word", "Octopus"],
+        ["pairs", "Octopus"]]
+
+    console.log(secretWordPairs);
+
+
     localStorage.setItem("usersData", JSON.stringify(users));
+    localStorage.setItem("secretWordPairs", JSON.stringify(secretWordPairs));
     console.log(localStorage)
+
+
+
+
+    function submitSecretWords() {
+        event.preventDefault();
+        
+        let word1 = document.getElementById("word1").value;
+        let word2 = document.getElementById("word2").value;
+
+
+        secretWordPairs.push([word1, word2])
+        localStorage.setItem("secretWordPairs", JSON.stringify(secretWordPairs));
+
+        console.log("Local Storage Updated!!!", localStorage)
+        
+    }
