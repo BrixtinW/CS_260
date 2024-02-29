df0afec7b0373b024dacbb1af2da2dd51e983dc0
BrixtinW <brixtinlwalker@gmail.com>
2024-02-28 18:30:23 -0700

    voting logic helped and registration info ironed out

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 // document.addEventListener("DOMContentLoaded", function() {
+    localStorage.clear();
+
     let users = [
         { username: "user1", password: "password1" },
         { username: "user2", password: "password2" }
@@ -7,7 +9,9 @@
 
     console.log(users);
 
+
     localStorage.setItem("usersData", JSON.stringify(users));
+    console.log(localStorage)
 // })
 
 // function selectPlayer(myName, playerName){
