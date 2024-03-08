c0c0c14129e385f9c8f78ceeb782c6b897212281
BrixtinW <brixtinlwalker@gmail.com>
2024-03-07 17:46:16 -0700

    reorganinzing files

---
+++ public/index.js
@@ -0,0 +1,39 @@
+    localStorage.clear();
+
+    let users = [
+        { username: "user1", password: "password1" },
+        { username: "user2", password: "password2" }
+    ];
+
+    console.log(users);
+
+
+    let secretWordPairs = [
+        ["secret", "Octopus"],
+        ["word", "Octopus"],
+        ["pairs", "Octopus"]]
+
+    console.log(secretWordPairs);
+
+
+    localStorage.setItem("usersData", JSON.stringify(users));
+    localStorage.setItem("secretWordPairs", JSON.stringify(secretWordPairs));
+    console.log(localStorage)
+
+
+
+
+    function submitSecretWords() {
+        event.preventDefault();
+
+        let word1 = document.getElementById("word1").value;
+        let word2 = document.getElementById("word2").value;
+
+        secretWordPairs.push([word1, word2])
+        localStorage.setItem("secretWordPairs", JSON.stringify(secretWordPairs));
+
+        alert(`Secret word candidates "${word1}" and "${word2}" submitted!`)
+
+        console.log("Local Storage Updated!!!", localStorage)
+        
+    }
