9617d1748be0f7a0833c7549af522474c4aecc47
BrixtinW <brixtinlwalker@gmail.com>
2024-03-09 12:11:46 -0700

    added new depolyment file

--- index.js
+++ index.js
@@ -1,39 +1,73 @@
-    localStorage.clear();
+const { randomUUID } = require('crypto');
+const express = require('express');
+const app = express();
 
-    let users = [
-        { username: "user1", password: "password1" },
-        { username: "user2", password: "password2" }
-    ];
+// The service port. In production the frontend code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-    console.log(users);
+// JSON body parsing using built-in middleware
+app.use(express.json());
 
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
 
-    let secretWordPairs = [
-        ["secret", "Octopus"],
-        ["word", "Octopus"],
-        ["pairs", "Octopus"]]
-
-    console.log(secretWordPairs);
-
-
-    localStorage.setItem("usersData", JSON.stringify(users));
-    localStorage.setItem("secretWordPairs", JSON.stringify(secretWordPairs));
-    console.log(localStorage)
+// Router for service endpoints
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 
 
+apiRouter.post('/gameroom', (req, res) => {
+    gameRoomCode = createGameRoom();
+    res.send(gameRoomCode);
+  });
 
-    function submitSecretWords() {
-        event.preventDefault();
+  apiRouter.post('/addPlayer', (req, res) => {
+    addPlayer(req.body, votes);
+    res.send(addedPlayer);
+  });
 
-        let word1 = document.getElementById("word1").value;
-        let word2 = document.getElementById("word2").value;
+apiRouter.get('/verdict', (_req, res) => {
+  res.send(votes);
+});
 
-        secretWordPairs.push([word1, word2])
-        localStorage.setItem("secretWordPairs", JSON.stringify(secretWordPairs));
+apiRouter.post('/vote', (req, res) => {
+  votes = updateVotes(req.body, votes);
+  res.send(scores);
+});
 
-        alert(`Secret word candidates "${word1}" and "${word2}" submitted!`)
 
-        console.log("Local Storage Updated!!!", localStorage)
-        
-    }
+
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
+votes = {}
+
+function createGameRoom() {
+    const code = generateGameRoomCode()
+    votes[code] = {};
+    return code;
+}
+
+function generateGameRoomCode() {
+    const uuid = randomUUID();
+    const code = uuid.replace(/-/g, '').substring(0, 6);
+    return code;
+  }
+
+ function addPlayer(reqBody, votes){
+    console.log(reqBody)
+    var json = JSON.parse(reqBody);
+    const code = json.code;
+    const name = json.name
+
+    votes[code][name] = 0;
+ }
