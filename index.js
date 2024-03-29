4922e66aaa20ad986c3b45314a0c5cb311a4eeb9
BrixtinW <brixtinlwalker@gmail.com>
2024-03-28 20:37:22 -0600

    updating websocket server and client code

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require('express');
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
-const { WebSocketServer } = require('ws');
 const app = express();
+const { peerProxy } = require('websocket.js');
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
@@ -31,7 +31,7 @@ apiRouter.get('/gameroom', (req, res) => {
   });
 
   apiRouter.post('/addPlayer', (req, res) => {
-    addPlayer(req.body);
+    addPlayer(req.body, req.header);
   });
 
   apiRouter.post('/players', (req, res) => {
@@ -107,18 +107,23 @@ apiRouter.post('/vote', (req, res) => {
 apiRouter.post('/generateOddOneOut', (req, res) => {
     generateOddOneOut(req.body);
   });
+  
+  
+  app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+  
+  const httpServer = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+  
+  // httpServer.on('upgrade', (request, socket, head) => {
+  //     this.wss.handleUpgrade(request, socket, head, (ws) => {
+  //         this.wss.emit('connection', ws, request);
+  //     });
+  // });
 
-
-// Return the application's default page if the path is unknown
-app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
-});
-
-const httpServer = app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-//////////////////////////////////////////// GAME LOGIC /////////////////////////////////////////////////
+  //////////////////////////////////////////// GAME LOGIC /////////////////////////////////////////////////
 
 
 class GameRoom {
@@ -132,56 +137,154 @@ class GameRoom {
         this.votes = new Map();
         this.readyToStartGame = false;
         this.gameOver = false;
-    }
+        this.connections = {};
+        // this.initializeWebSocketServer(httpServer);
+      }
 
-    toString(){
+//       initializeWebSocketServer(httpServer) {
+//         this.wss = new WebSocketServer({ noServer: true });
+    
+//         this.wss.on('connection', (ws) => {
+//           ws.on('message', (data) => {
+//               // Handle incoming messages
+//               const messageData = JSON.parse(data);
+//               if (messageData.name) {
+//                   const name = messageData.name;
+//                   this.connections[name] = { alive: true, ws: ws };
+//                   console.log(`Added ${name} to connections`);
+
+//                   // Notify other players about the new player
+//                   // this.broadcastMessageExcept(name, { name: `${name}` });
+//               } else {
+//                   // Handle other types of messages
+//               }
+//           });
+    
+//             ws.on('close', () => {
+//                 // Handle connection closing
+//                 const name = Object.keys(this.connections).find(key => this.connections[key].ws === ws);
+//                 if (name) {
+//                     delete this.connections[name];
+//                 }
+//             });
+    
+//             ws.on('pong', () => {
+//                 // Handle pong messages
+//                 const name = Object.keys(this.connections).find(key => this.connections[key].ws === ws);
+//                 if (name && this.connections[name]) {
+//                     this.connections[name].alive = true;
+//                 }
+//             });
+//         });
+
+//         httpServer.on('upgrade', (request, socket, head) => {
+//           this.wss.handleUpgrade(request, socket, head, (ws) => {
+//               this.wss.emit('connection', ws, request);
+//           });
+//       });
+    
+//         // Keep active connections alive
+//         setInterval(() => {
+//             Object.keys(this.connections).forEach((name) => {
+//                 const connection = this.connections[name];
+//                 if (!connection.alive) {
+//                     connection.ws.terminate();
+//                     delete this.connections[name];
+//                 } else {
+//                     connection.alive = false;
+//                     connection.ws.ping();
+//                 }
+//             });
+//         }, 10000);
+//     }
+    
+
+//     createConnection(name) {
+//       const ws = new WebSocket(`ws://localhost:${port}`);
+//       // const ws = new WebSocket(`ws://startup.odd-one-out.click:${port}`);
+
+//       ws.on('open', () => {
+//         // Send the player's name to the server
+//         ws.send(JSON.stringify({ name: name }));
+//         // Emit custom event to signal connection creation
+//         this.wss.emit('addConnection', ws, name);
+//     });
+
+//       ws.on('message', (data) => {
+//           // Handle incoming messages
+//           console.log(data);
+//       });
+
+//       ws.on('close', () => {
+//           // Handle connection closing
+//           console.log('Connection closed');
+//       });
+
+//       ws.on('error', (error) => {
+//           // Handle connection errors
+//           console.error('Connection error:', error);
+//       });
+
+//   }
+
+//   broadcastMessageExcept(excludedPlayer, messageObject) {
+//     Object.keys(this.connections).forEach((name) => {
+//         if (name !== excludedPlayer) {
+//             this.connections[name].ws.send(JSON.stringify(messageObject));
+//         }
+//     });
+// }
+
+    
+      
+      toString(){
         if(this.readyToStartGame){
-            return `\tPlayers = ${this.players}\n\toddOneOut = ${players[this.oddOneOutIndex]}\n\tGame Room Code = ${this.code}\n\tVotes = ${this.votes}\n\tgroupWord = ${this.groupWord}\n\toddWord = ${this.oddWord}\n\tready to start: ${this.readyToStartGame}`;
+          return `\tPlayers = ${this.players}\n\toddOneOut = ${players[this.oddOneOutIndex]}\n\tGame Room Code = ${this.code}\n\tVotes = ${this.votes}\n\tgroupWord = ${this.groupWord}\n\toddWord = ${this.oddWord}\n\tready to start: ${this.readyToStartGame}`;
         } else {
-            return `\tPlayers = ${this.players}\n\toddOneOut = Not Yet Determined!\n\tGame Room Code = ${this.code}\n\tVotes = ${this.votes}\n\tgroupWord = ${this.groupWord}\n\toddWord = ${this.oddWord}\n\tready to start: ${this.readyToStartGame}`;
+          return `\tPlayers = ${this.players}\n\toddOneOut = Not Yet Determined!\n\tGame Room Code = ${this.code}\n\tVotes = ${this.votes}\n\tgroupWord = ${this.groupWord}\n\toddWord = ${this.oddWord}\n\tready to start: ${this.readyToStartGame}`;
         }
-    }
-
-    generateOddOneOutIndex(){
+      }
+      
+      generateOddOneOutIndex(){
         const randomNum = Math.floor(Math.random() * this.players.length); 
         this.oddOneOutIndex = randomNum;
+      }
+      
     }
-
-}
-
+    
     const games = new Map();
-
-let secretWordPairs = [
-    ["Squid", "Octopus"],
-    ["Queen", "Princess"],
-    ["Trophy", "Medal"]]
-
-
-function getSecretWord(reqBody){
-    console.log(reqBody)
-    const code = reqBody.code;
-    const name = reqBody.name;
+    
+    let secretWordPairs = [
+      ["Squid", "Octopus"],
+      ["Queen", "Princess"],
+      ["Trophy", "Medal"]]
+      
+      
+      function getSecretWord(reqBody){
+        console.log(reqBody)
+        const code = reqBody.code;
+        const name = reqBody.name;
     console.log(games.get(code).oddOneOutIndex);
-        if( games.get(code).readyToStartGame == true){
-            console.log(games.get(code).players[games.get(code).oddOneOutIndex])
-            if (games.get(code).players[games.get(code).oddOneOutIndex] == name){
-                console.log(games.get(code).oddWord)
-                return games.get(code).oddWord;
-            } else {
-                console.log(games.get(code).groupWord)
-                return games.get(code).groupWord;
-            }
-        }
-}
-
-function generateOddOneOut(reqBody){
+    if( games.get(code).readyToStartGame == true){
+      console.log(games.get(code).players[games.get(code).oddOneOutIndex])
+      if (games.get(code).players[games.get(code).oddOneOutIndex] == name){
+        console.log(games.get(code).oddWord)
+        return games.get(code).oddWord;
+      } else {
+        console.log(games.get(code).groupWord)
+        return games.get(code).groupWord;
+      }
+    }
+  }
+  
+  function generateOddOneOut(reqBody){
     console.log(reqBody)
     const code = reqBody.code;
     games.get(code).generateOddOneOutIndex();
     games.get(code).readyToStartGame = true;
-}
-
-function createGameRoom() {
+  }
+  
+  function createGameRoom() {
     const code = generateGameRoomCode()
     const[groupWord, oddWord] = organizeSecretWords();
     console.log(groupWord);
@@ -190,47 +293,74 @@ function createGameRoom() {
     games.set(code, new GameRoom(code, groupWord, oddWord));
     console.log(games.get(code).toString());
     return JSON.stringify(code);
-}
-
-function generateGameRoomCode() {
+  }
+  
+  function generateGameRoomCode() {
     const uuid = randomUUID();
     const code = uuid.replace(/-/g, '').substring(0, 6);
     return code;
   }
-
- function addPlayer(reqBody){
-    console.log(reqBody)
+  
+  function addPlayer(reqBody, reqHeaders) {
+    console.log(reqBody);
     const code = reqBody.code;
     const name = reqBody.name;
+    
 
-    games.get(code).players.push(name);
-    // send message to update each players html with the correct names. index all sesssions with the game room code. update every connection in the gameroom with that message. 
-    console.log(games.get(code).toString());
- }
+    const game = games.get(code);
+    game.players.push(name);
 
- async function getUser(username) {
+    game.createConnection(name);
+
+    
+    // const ws = new WebSocket(`ws://${reqHeaders.host}`);
+    
+    // ws.on('message', function message(data) {
+    //   Object.entries(game.connections).forEach(([playerName, c]) => {
+    //     if (playerName !== name) {
+    //       c.send(data);
+    //     }
+    //   });
+    // });
+    // ws.on('close', () => {
+    //   console.log(`Connection closed for player ${name}`);
+    //   delete game.connections[name];
+    // });
+    
+    // game.connections[name] = ws;
+    
+    // Object.values(game.connections).forEach((connection) => {
+    //   connection.send(JSON.stringify({ type: 'addedPlayer', playerName: name }));
+    // });
+    
+    console.log(game.toString());
+    console.log(game.connections);
+  }
+
+  
+  async function getUser(username) {
     return collection.findOne({ username: username });
- }
-
- async function createUser(username, password) {
-
+  }
+  
+  async function createUser(username, password) {
+    
     const passwordHash = await bcrypt.hash(password, 10);
-
+    
     const user = {
-        username: username,
-        password: passwordHash,
-        token: uuid.v4()
-      };
-      return collection.insertOne(user);
- }
-
-
- function organizeSecretWords(){
+      username: username,
+      password: passwordHash,
+      token: uuid.v4()
+    };
+    return collection.insertOne(user);
+  }
+  
+  
+  function organizeSecretWords(){
     const randomNum = Math.floor(Math.random() * secretWordPairs.length); 
     return [secretWordPairs[randomNum][0], secretWordPairs[randomNum][1]];
- }
-
- function updateVotes(requestBody){
+  }
+  
+  function updateVotes(requestBody){
     let winners = null;
     const voter = requestBody.voter;
     const vote = requestBody.vote;
@@ -238,44 +368,44 @@ function generateGameRoomCode() {
     games.get(code).votes.set(voter, vote);
     console.log(games.get(code).votes);
     if (games.get(code).players.length >= Map.size){
-        winners = readTheVotes(code);
-        for (const winner of winners) {
-            if (games.get(code).players[games.get(code).oddOneOutIndex] == winner) { 
-                games.get(code).gameOver = true; 
-            } else {
-            const oddOneOut = games.get(code).players[games.get(code).oddOneOutIndex]
-            games.get(code).players = games.get(code).filter(value => value !== winner);
-            games.get(code).vote.clear();
-            for (let i = 0; i < games.get(code).players.size; i++){
-                if (games.get(code).players[i] == oddOneOut){
-                    games.get(code).oddOneOutIndex = i;
-                }
+      winners = readTheVotes(code);
+      for (const winner of winners) {
+        if (games.get(code).players[games.get(code).oddOneOutIndex] == winner) { 
+          games.get(code).gameOver = true; 
+        } else {
+          const oddOneOut = games.get(code).players[games.get(code).oddOneOutIndex]
+          games.get(code).players = games.get(code).filter(value => value !== winner);
+          games.get(code).vote.clear();
+          for (let i = 0; i < games.get(code).players.size; i++){
+            if (games.get(code).players[i] == oddOneOut){
+              games.get(code).oddOneOutIndex = i;
             }
-            }
+          }
         }
+      }
     }
     return winners;
-}
-
-
-function readTheVotes(code){
+  }
+  
+  
+  function readTheVotes(code){
     const voteCounts = new Map();
     
-
+    
     for (const [player, votedFor] of games.get(code).votes) {
-        for (const votedPlayer of votedFor.values()) {
-            const count = voteCounts.get(votedPlayer);
-            voteCounts.set(votedPlayer, count + 1);
-        }
+      for (const votedPlayer of votedFor.values()) {
+        const count = voteCounts.get(votedPlayer);
+        voteCounts.set(votedPlayer, count + 1);
+      }
     }
     
-
+    
     const maxVotes = Math.max(...voteCounts.values());
     const winners = [];
     for (const [player, votes] of voteCounts) {
-        if (votes === maxVotes) {
-            winners.push(player);
-        }
+      if (votes === maxVotes) {
+        winners.push(player);
+      }
     }
     
     // Return the winner(s)
@@ -314,57 +444,57 @@ function setAuthCookie(res, authToken) {
   }
 
 //////////////////////////////////////////// WEBSOCKET CODE /////////////////////////////////////////////////
+// function createWebSocket(httpServer, code, name) {
 
-  // Create a websocket object
-  const wss = new WebSocketServer({ noServer: true });
+//   const game = games.get(code);
+  
 
-  // Handle the protocol upgrade from HTTP to WebSocket
-  httpServer.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, function done(ws) {
-      wss.emit('connection', ws, request);
-    });
-  });
+//   const wss = new WebSocket.Server({ noServer: true });
 
-  // Keep track of all the connections so we can forward messages
-  let connections = [];
 
-  wss.on('connection', (ws) => {
-    const connection = { id: uuid.v4(), alive: true, ws: ws };
-    connections.push(connection);
+//   httpServer.on('upgrade', (request, socket, head) => {
+//       wss.handleUpgrade(request, socket, head, function done(ws) {
+//           wss.emit('connection', ws, request);
+//       });
+//   });
 
-    // Forward messages to everyone except the sender
-    ws.on('message', function message(data) {
-      connections.forEach((c) => {
-        if (c.id !== connection.id) {
-          c.ws.send(data);
-        }
-      });
-    });
 
-    // Remove the closed connection so we don't try to forward anymore
-    ws.on('close', () => {
-      const pos = connections.findIndex((o, i) => o.id === connection.id);
+//   wss.on('connection', (ws) => {
 
-      if (pos >= 0) {
-        connections.splice(pos, 1);
-      }
-    });
+//       game.connections[name] = ws;
 
-    // Respond to pong messages by marking the connection alive
-    ws.on('pong', () => {
-      connection.alive = true;
-    });
-  });
 
-  // Keep active connections alive
-  setInterval(() => {
-    connections.forEach((c) => {
-      // Kill any connection that didn't respond to the ping last time
-      if (!c.alive) {
-        c.ws.terminate();
-      } else {
-        c.alive = false;
-        c.ws.ping();
-      }
-    });
-  }, 10000);
+//       ws.on('message', function message(data) {
+//           Object.entries(game.connections).forEach(([playerName, c]) => {
+//               if (playerName !== name) {
+//                   c.send(data);
+//               }
+//           });
+//       });
+
+
+//       ws.on('close', () => {
+//           delete game.connections[name];
+//       });
+
+
+//       ws.on('pong', () => {
+//           game.connections[connectionId].alive = true;
+//       });
+//   });
+
+//   setInterval(() => {
+//       Object.values(game.connections).forEach((c) => {
+//           if (!c.alive) {
+//               c.terminate();
+//           } else {
+//               c.alive = false;
+//               c.ping();
+//           }
+//       });
+//   }, 10000);
+
+
+//   return wss;
+// }
+peerProxy(httpServer, games);
